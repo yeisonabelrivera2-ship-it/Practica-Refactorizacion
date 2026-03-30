32e5f7e549b24b85b96a9cfccfdb9e60c4718864
papu <papu@gmail.com>
2026-03-30 17:09:23 +0200

    ProcesadorFacturas-v2

--- src/github/ProcesadorFacturas.java
+++ src/github/ProcesadorFacturas.java
@@ -8,9 +8,7 @@ public class ProcesadorFacturas {
 	public void procesarFactura (List<Double> precios, String nombreCliente, double impuesto, boolean vip) {
 	double total = 0;
 	// SMELL: Lógica de cálculo mezclada con impresión (Método Largo)
-	for (Double d : precios) {
-	total += d;
-	}
+	total = CalcularSumarBase(precios, total);
 	// SMELL: Código duplicado y números "mágicos" (0.10, 0.05)
 	if (vip) {
 	total = total - (total * 0.10);
@@ -22,6 +20,13 @@ public class ProcesadorFacturas {
 	System.out.println("Total con impuestos: " + total);
 	}
 	// SMELL: Más código duplicado (la impresión del nombre)
+	}
+
+	private double CalcularSumarBase(List<Double> precios, double total) {
+		for (Double d : precios) {
+		total += d;
+		}
+		return total;
 	}
 	}
 
